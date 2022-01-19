26405638397eb695e529b097bb6c897611264a4f
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-01-19 09:03:16 +0530

    Create dataTypes.go

---
+++ dataTypes.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+//main function will be automatically called when you start the appplication
+func main() {
+	var data int
+	data = 10
+
+	data1 := 100 //declare(automatic datatype) the variable and asign value to it
+	//	data := 1000//
+	data = 1000
+	fmt.Println(data)
+	fmt.Println(data1)
+
+	//data types
+	//1. Primitive
+	// int, float, string, bool
+	//2. Non-Primitive
+	// struct
+	//functional programming
+	//recent feature update of java- from java 11
+}
