8ccc5891c5c66363b5d8980518a999416de96a00
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-01-20 09:21:44 +0530

    Update greetingfunc.go

--- greetingfunc.go
+++ greetingfunc.go
@@ -1,5 +1,23 @@
 package main
 
-func greetings(name string) string {
-	return "hello," + name
+import (
+	"errors"
+	"fmt"
+)
+
+func greetings(name string) (string, error) {
+	if name == "" {
+		return name, errors.New(" name cannot be empty!")
+	}
+	return "hello," + name, nil
+}
+
+func main() {
+	//fmt.Println(greetings("Maurice"))
+	greeting, err := greetings("Maurice Yeng")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(greeting)
+	}
 }
