cbec31ba6c29e7839923394c510c49a805c92142
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-01-20 20:37:30 +0530

    Update day2_1.go

--- practice/day2_1.go
+++ practice/day2_1.go
@@ -1,8 +1,33 @@
 package main
 
-func factorial(number int)(fact int, error) {
+import (
+	"errors"
+	"fmt"
+)
 
+func factorial(number int) (fact int, err error) {
+	fact = 1
+	if number < 0 {
+		err = errors.New("number has to be positive")
+		return
+	} else if number == 0 {
+		return
+	}
+	for number > 0 {
+		fact *= number
+		number--
+	}
+	return
 }
 func main() {
+	var num int
+	fmt.Println("enter number: ")
+	fmt.Scanln(&num)
+	result, err := factorial(num)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("the factorial is: ", result)
+	}
 
 }
