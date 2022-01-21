8fa42b284ec84d154462dbc911c7cece1087ab9c
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-01-21 09:12:08 +0530

    Create closure.go

---
+++ closure.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+//function is a first class variable in GoLang
+
+func main() {
+	//number := 10
+	var number int
+	number = 10
+	fmt.Println(number)
+
+	//store a function as a value to a variable
+	var getInt func(int) int
+	getInt = func(x int) int {
+		fmt.Println("In a function")
+		return 10 + x
+	}
+	getInt(90)
+
+}
