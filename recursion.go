2e968b3f11b2cef726ee17d8d20cd73b3bd8b64f
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-01-22 08:23:41 +0530

    Update recursion.go

--- recursion.go
+++ recursion.go
@@ -1,11 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 
 }
 func fact(number int) int {
 	if number == 1 || number == 0 {
 		return 1
+	}
+	if number < 0 {
+		fmt.Println("invalid number")
+		return 0
 	}
 	result := number * fact(number-1)
 	return result
