c90ed518180a1ee20ac0a7e6534391c2a26b9b45
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-01-24 08:44:04 +0530

    Update array.go

--- array.go
+++ array.go
@@ -1,9 +1,16 @@
 package main
 
-var arr[4]int
+import "fmt"
 
-func main(){
-	arr={1,2,3,4,5}
+//var arr [4]int
+
+func main() {
+	/*
+		arr[0] = 1
+		arr[1] = 2
+		arr[2] = 3
+	*/
+	arr := [...]int{1, 2, 3, 4, 5, 6}
 
 	fmt.Println(arr)
 
