3c5513cca79879988be9b62e2dbbe2716037e3e8
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-01-24 21:09:50 +0530

    Update chessboard.go

--- practice/chessboard.go
+++ practice/chessboard.go
@@ -1,16 +1,28 @@
 package main
 
+import "fmt"
+
 var chessBoard [8][8]string
 
-func checkOccupied(rank int)int {
-	count:=0
-	for _, value:=chessBoard[rank]{
-		if value=="#"{
+func checkOccupied(rank int) int {
+	count := 0
+	for _, value := range chessBoard[rank] {
+		if value == "#" {
 			count++
 		}
 	}
+	return count
 }
+func checkOccupiedFiles(files int) int {
+	count := 0
 
+	for index1, _ := range chessBoard {
+		if chessBoard[index1][file] == "#" {
+			count++
+		}
+	}
+	return count
+}
 func main() {
 	chessBoard = [8][8]string{
 		{"#", "_", "#", "_", "_", "_", "_", "_"},
@@ -21,4 +33,6 @@ func main() {
 		{"_", "#", "#", "_", "_", "_", "_", "_"},
 		{"_", "_", "_", "_", "_", "_", "_", "#"},
 		{"_", "_", "#", "_", "_", "_", "_", "_"}}
+
+	fmt.Println(checkOccupied(0))
 }
