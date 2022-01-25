2472a68dddc0e2bf9327fb1ec4a4cc518c80ad65
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-01-25 08:48:53 +0530

    Create waterBlock.go

---
+++ practice/geeksforgeeks/waterBlock.go
@@ -0,0 +1,18 @@
+package main
+
+var arr = []int{3, 0, 0, 2, 0, 4}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func volume(h int) int {
+	return h
+}
+
+func main() {
+
+}
