08ca14a7e84b52f5861b379a5bf86793fab7d9a1
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-01-25 09:21:28 +0530

    Update waterBlock.go

--- practice/geeksforgeeks/waterBlock.go
+++ practice/geeksforgeeks/waterBlock.go
@@ -1,6 +1,7 @@
 package main
 
 var arr = []int{3, 0, 0, 2, 0, 4}
+var maxHeight int
 
 func min(x, y int) int {
 	if x < y {
@@ -14,5 +15,21 @@ func volume(h int) int {
 }
 
 func main() {
+	maxHeight = min(arr[0], arr[len(arr)-1])
+	waterCaptured := 0
+	for index, value := range arr {
+		if index > 0 && index < len(arr) {
+			heightLeft := arr[index-1]
+			heightRight := arr[index+1]
+			if heightLeft > maxHeight {
+				heightLeft = maxHeight
 
+			}
+			if heightRight > maxHeight {
+				heightRight = maxHeight
+
+			}
+			waterCaptured += volume(min(heightLeft, heightRight))
+		}
+	}
 }
