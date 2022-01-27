b332b3c5566f4d284a2dd64059410d796d622b54
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-01-27 19:51:53 +0530

    Update interface2.go

--- interface2.go
+++ interface2.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
+type SalaryCalculator interface {
+}
+
+func describe(i interface{}) {
+	fmt.Printf("Type= %T,value=%v", i, i)
+	var num int
+	num = i.(int)
+
+}
+
 func main() {
+	describe("Hello")
+	describe(12)
+	describe(true)
 
 }
