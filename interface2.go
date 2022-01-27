ecaa455c60c5529978b928f82d504213385daf80
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-01-27 20:14:17 +0530

    Update interface2.go

--- interface2.go
+++ interface2.go
@@ -6,15 +6,18 @@ type SalaryCalculator interface {
 }
 
 func describe(i interface{}) {
-	fmt.Printf("Type= %T,value=%v", i, i)
+	fmt.Printf("Type= %T,value=%v\n", i, i)
 	var num int
+	var ok bool
 	num = i.(int)
+	fmt.Println(num)
+	fmt.Println(ok)
 
 }
 
 func main() {
 	describe("Hello")
-	describe(12)
-	describe(true)
+	//describe(12)
+	//describe(true)
 
 }
