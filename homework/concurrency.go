c3a431cdeab5df07d9db565f3abf2868fd0b237f
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-02-03 19:24:22 +0530

    Update concurrency.go

--- homework/concurrency.go
+++ homework/concurrency.go
@@ -1,14 +1,21 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	var mutex sync.Mutex
 	counter := 0
 	for i := 0; i < 10; i++ {
 		go func() {
+			mutex.Lock()
 			counter++
-		}()
-	}
+			mutex.Unlock()
 
+		}()
+		fmt.Println(counter)
+	}
+	//fmt.Println(counter)
 }
