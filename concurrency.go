2f61a958e87432062214076e5da1639b7d56f066
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-02-03 19:40:48 +0530

    Update concurrency.go

--- concurrency.go
+++ concurrency.go
@@ -3,17 +3,19 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 var wait sync.WaitGroup
 var counter int
-func add(hint string){
-	for i:=0;i<10;i++{
-		a:=counter
+
+func add(hint string) {
+	for i := 0; i < 3; i++ {
+		a := counter
 		a++
-		time.Sleep(time.Second*2)
-		counter=a
-		fmt.Println(hint,"i:=",i," Count:=",counter)
+		time.Sleep(time.Second * 2)
+		counter = a
+		fmt.Println(hint, "i:=", i, " Count:=", counter)
 	}
 	wait.Done()
 }
@@ -22,4 +24,6 @@ func main() {
 	go add("first: ")
 	go add("second: ")
 	wait.Wait()
-	fmt.Println("final value of counter:= ",counter)
+	fmt.Println("final value of counter:= ", counter)
+
+}
