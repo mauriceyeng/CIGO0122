d43643b09270e2c2b81d6f2196ecd80dbf6acd4d
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-02-09 08:16:05 +0530

    Update context.go

--- context.go
+++ context.go
@@ -4,4 +4,7 @@ func main() {
 	//store and pass the info across the different layers of application
 	//context variable and pass it through the main->router->handler->db function
 
+	//the ability to cancellation of the job in between the execution
+	//(rahul sir's task) consume restful api.. and if not done with it 2minutes then job needs to be cancelled
+
 }
