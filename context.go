6ca8b3744d58a89b1981720c97fb1214a64ceaf4
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-02-09 08:15:01 +0530

    context

---
+++ context.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	//store and pass the info across the different layers of application
+	//context variable and pass it through the main->router->handler->db function
+
+}
