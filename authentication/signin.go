613c84c850fcdfb317c318a314c9f97b7b9cb375
Maurice Yengkhom <56469940+mauriceyeng@users.noreply.github.com>
2022-02-10 09:46:48 +0530

    Update signin.go

--- authentication/signin.go
+++ authentication/signin.go
@@ -2,3 +2,11 @@
 // Request method
 // Request schema: param, query, body     {mailid,password}
 // Response schema: Token{unique string} for authentication
+
+/*
+QUERY:
+	https://localhost:8080/book/11		- params
+	https://localhost:8080/book?id=11	- query
+
+
+*/
